eb7246a53f2711c63572ecfbbe00690c73847867
Sagar <sagarkirtakar2002@gmail.com>
2025-10-01 11:25:12 +0530

    First commit

---
+++ Lecture Code/src/in/sagarkirtakar/HelloWorld/Hello.java
@@ -0,0 +1,8 @@
+package in.sagarkirtakar.HelloWorld;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to java programming....");
+    }
+}
